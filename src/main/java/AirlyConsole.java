a32a50a6c6bf3ec6441ccf06d1eb161a44794aba
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2018-01-23 00:42:30 +0100

    Added command line handling

--- src/main/java/AirlyConsole.java
+++ src/main/java/AirlyConsole.java
@@ -1,5 +1,101 @@
+import com.martiansoftware.jsap.JSAPResult;
+
+import javax.naming.AuthenticationException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class AirlyConsole {
-    
+    private WebReader webReader;
+
+
+    public void execute(JSAPResult jsapResult) {
+        if (jsapResult.contains("api-key")) {
+            String apiKey = jsapResult.getString("api-key");
+            webReader = new WebReader(apiKey);
+        } else {
+            try {
+                webReader = new WebReader();
+            } catch (IllegalStateException e) {
+                System.out.println("Cannot find API KEY.");
+                return;
+            } catch (SecurityException e) {
+                System.out.println("Access to env API Key access is forbidden.");
+                return;
+            }
+        }
+        boolean showHistory = jsapResult.getBoolean("history");
+
+        if (jsapResult.contains("sensor-id")) {
+            int sensorId = jsapResult.getInt("sensor-id");
+            if (!showHistory) {
+                showSensorCurrentMeasurements(sensorId);
+            } else {
+                showSensorHistoricMeasurements(sensorId);
+            }
+        } else {
+            if (!jsapResult.contains("latitude") || !jsapResult.contains("longitude")) {
+                System.out.println("Program requires both latitude and longitude.");
+                return;
+            }
+            double latitude = jsapResult.getDouble("latitude");
+            double longitude = jsapResult.getDouble("longitude");
+            if (!showHistory) {
+                showNearestSensorCurrentMeasurements(latitude, longitude);
+            } else {
+                showNearestSensorHistoricMeasurements(latitude, longitude);
+            }
+        }
+    }
+
+    public void showSensorCurrentMeasurements(int sensorId) {
+        SensorMeasurements sensorMeasurements =readAndHandleSensorMeasurements(sensorId);
+        if (sensorMeasurements == null)
+            return;
+        printCurrentSensorMeasurements(sensorMeasurements.currentMeasurements);
+    }
+
+    public void showSensorHistoricMeasurements(int sensorId) {
+        SensorMeasurements sensorMeasurements =readAndHandleSensorMeasurements(sensorId);
+        if (sensorMeasurements == null)
+            return;
+        printHistoricSensorMeasurements(sensorMeasurements.history);
+    }
+
+    private SensorMeasurements readAndHandleSensorMeasurements(int sensorId){
+        SensorMeasurements sensorMeasurements;
+        try {
+            sensorMeasurements = webReader.readSensorMeasurements(sensorId, 24, 1);
+        } catch (URISyntaxException e) {
+            System.out.println("Couldn't create URI.");
+            return null;
+        } catch (IOException e) {
+            System.out.println("Couldn't establish connection with server.");
+            return null;
+        } catch (AuthenticationException e) {
+            System.out.println("API Key is not valid.");
+            return null;
+        }
+        return sensorMeasurements;
+    }
+
+    public void showNearestSensorCurrentMeasurements(double latitude, double longitude) {
+
+    }
+
+    public void showNearestSensorHistoricMeasurements(double latitude, double longitude) {
+
+    }
+
+    private void printCurrentSensorMeasurements(Measurements sensorMeasurements) {
+        System.out.println(sensorMeasurements.airQualityIndex);
+        System.out.println(sensorMeasurements.pm1);
+        System.out.println(sensorMeasurements.pm10);
+        System.out.println(sensorMeasurements.pm25);
+        System.out.println(sensorMeasurements.temperature);
+    }
+
+    private void printHistoricSensorMeasurements(MeasurementsWithInterval[] sensorMeasurements) {
+
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,67 @@
-import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.*;
 
 public class Main {
     public static void main(String[] args){
         JSAP jsap = new JSAP();
-        JSAPResult result = jsap.parse(args);
+        JSAPResult jsapResult;
+        try {
+            fillJsapParser(jsap);
+        }
+        catch (JSAPException e){
+            System.out.println("There was a problem with JSAP parser.");
+            return;
+        }
+        try {
+            jsapResult = jsap.parse(args);
+        }
+        catch (Exception e){
+            System.out.println("Couldn't parse given options.");
+            return;
+        }
+        AirlyConsole airlyConsole = new AirlyConsole();
+        airlyConsole.execute(jsapResult);
     }
 
-    private void fillJsapParser()
+    private static void fillJsapParser(JSAP jsap) throws JSAPException{
+        FlaggedOption apiKeyOption = new FlaggedOption("api-key")
+                .setStringParser(JSAP.STRING_PARSER)
+                .setShortFlag(JSAP.NO_SHORTFLAG)
+                .setLongFlag("api-key");
+
+        apiKeyOption.setHelp("Program will use given api key to authenticate. " +
+                "If no api key was specified, program will try to use env viable API_KEY.");
+
+        FlaggedOption sensorIdOption = new FlaggedOption("sensor-id")
+                .setStringParser(JSAP.INTEGER_PARSER)
+                .setShortFlag(JSAP.NO_SHORTFLAG)
+                .setLongFlag("sensor-id");
+
+        sensorIdOption.setHelp("Shows measurements from the sensor of given id.");
+
+        FlaggedOption latitudeOption = new FlaggedOption("latitude")
+                .setStringParser(JSAP.DOUBLE_PARSER)
+                .setShortFlag(JSAP.NO_SHORTFLAG)
+                .setLongFlag("latitude");
+
+        latitudeOption.setHelp("Shows measurements from the nearest sensor for given latitude. Requires longitude.");
+
+        FlaggedOption longitudeOption = new FlaggedOption("longitude")
+                .setStringParser(JSAP.DOUBLE_PARSER)
+                .setShortFlag(JSAP.NO_SHORTFLAG)
+                .setLongFlag("longitude");
+
+        longitudeOption.setHelp("Shows measurements from the nearest sensor for given longitude. Requires latitude.");
+
+        Switch historySwitch = new Switch("history")
+                .setShortFlag(JSAP.NO_SHORTFLAG)
+                .setLongFlag("history");
+
+        historySwitch.setHelp("Changes program mode to display historic measurements instead of current one.");
+
+        jsap.registerParameter(apiKeyOption);
+        jsap.registerParameter(sensorIdOption);
+        jsap.registerParameter(latitudeOption);
+        jsap.registerParameter(longitudeOption);
+        jsap.registerParameter(historySwitch);
+    }
 }
