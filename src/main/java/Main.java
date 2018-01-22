6af9c886f7e2398ee3f3950cfcbbe5574c7975d6
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2018-01-22 22:23:32 +0100

    Added gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPResult;
+
 public class Main {
+    public static void main(String[] args){
+        JSAP jsap = new JSAP();
+        JSAPResult result = jsap.parse(args);
+    }
+
+    private void fillJsapParser()
 }
