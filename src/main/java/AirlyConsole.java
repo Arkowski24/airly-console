346ef248729434ed6f50e55ceaa2b1abc6442275
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2018-01-23 10:24:33 +0100

    Added ASCII art

--- src/main/java/AirlyConsole.java
+++ src/main/java/AirlyConsole.java
@@ -2,7 +2,10 @@ import com.martiansoftware.jsap.JSAPResult;
 
 import javax.naming.AuthenticationException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AirlyConsole {
     private WebReader webReader;
@@ -87,15 +90,57 @@ public class AirlyConsole {
     }
 
     private void printCurrentSensorMeasurements(Measurements sensorMeasurements) {
-        System.out.println(sensorMeasurements.airQualityIndex);
-        System.out.println(sensorMeasurements.pm1);
-        System.out.println(sensorMeasurements.pm10);
-        System.out.println(sensorMeasurements.pm25);
-        System.out.println(sensorMeasurements.temperature);
+        List<String> baseAscii = getDanger();
+        for (String line : baseAscii){
+            System.out.println(line);
+        }
     }
 
     private void printHistoricSensorMeasurements(MeasurementsWithInterval[] sensorMeasurements) {
 
     }
 
+    private List<String> getGood(){
+        List<String> okAscii = new ArrayList<>();
+        okAscii.add("   ______                __");
+        okAscii.add("  / ____/___  ____  ____/ /");
+        okAscii.add(" / / __/ __ \\/ __ \\/ __  / ");
+        okAscii.add("/ /_/ / /_/ / /_/ / /_/ /  ");
+        okAscii.add("\\____/\\____/\\____/\\__,_/   ");
+        okAscii.add("                           ");
+        return okAscii;
+    }
+
+    private List<String> getOK(){
+        List<String> okAscii = new ArrayList<>();
+        okAscii.add("   ____  __  ");
+        okAscii.add("  / __ \\/ /__");
+        okAscii.add(" / / / / //_/");
+        okAscii.add("/ /_/ / ,<   ");
+        okAscii.add("\\____/_/|_|  ");
+        okAscii.add("             ");
+        return okAscii;
+    }
+
+    private List<String> getBad(){
+        List<String> okAscii = new ArrayList<>();
+        okAscii.add("    ____            __");
+        okAscii.add("   / __ )____ _____/ /");
+        okAscii.add("  / __  / __ `/ __  / ");
+        okAscii.add(" / /_/ / /_/ / /_/ /  ");
+        okAscii.add("/_____/\\__,_/\\__,_/   ");
+        okAscii.add("                      ");
+        return okAscii;
+    }
+
+    private List<String> getDanger(){
+        List<String> okAscii = new ArrayList<>();
+        okAscii.add("    ____  _   ________");
+        okAscii.add("   / __ \\/ | / / ____/");
+        okAscii.add("  / / / /  |/ / / __  ");
+        okAscii.add(" / /_/ / /|  / /_/ /  ");
+        okAscii.add("/_____/_/ |_/\\____/   ");
+        okAscii.add("                      ");
+        return okAscii;
+    }
 }
